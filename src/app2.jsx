6adf633351814fc8cbf6292ea0193c8109558843
Ian Fajardo <ifajardo@arccorp.com>
2023-08-02 15:41:00 -0400

    randomize quote

--- src/app2.jsx
+++ src/app2.jsx
@@ -8,23 +8,51 @@ import {
   ProductLinks,
 } from "arccorp-vars";
 
+var quotes = [
+  <ProductQuote
+    author="Simon Brooks"
+    title="Senior Vice President, Sales for North America"
+    backgroundImage="https://www2.arccorp.com/globalassets/products--participation/direct-connect/arc-dc-quote.png"
+    className="arc-dc-product-quote"
+    company="British Airways"
+    id="quote"
+    quote="British Airways is excited to be the first test partner for ARC's enhanced settlement functionality, which enables us to evolve our distribution strategy with  the travel agency channel. ARC has always been a trusted partner to British Airways, and we have valued  their flexibility, responsiveness and collaboration  during this testing process. These enhancements  will aid us in developing our relationships with  agencies, and they set the stage for our growing  distribution stratetgy in the U.S."
+  />,
+  <ProductQuote
+    author="Jenni Suomela"
+    title="Vice President of Global Sales and Channel Management"
+    backgroundImage="https://www2.arccorp.com/globalassets/products--participation/direct-connect/ndc-quote2.png"
+    className="arc-dc-product-quote"
+    company="Finnair"
+    id="quote2"
+    quote="We are happy to partner with ARC on this important milestone on our NDC journey. Adoption of NDC with ARC benefits our joint travel agency network and ultimately enhances customer experience and allows more choice for customers."
+  />,
+];
+
 class App2 extends Component {
   constructor() {
     super();
+    this.state = {
+      randomQuoteIndex: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ randomQuoteIndex: Math.floor(Math.random() * quotes.length) });
+  }
+
+  randomQuote() {
+    return quotes[this.state.randomQuoteIndex];
   }
 
   render() {
+    var visibleQuote = this.randomQuote();
+
+    console.log(this.state.randomQuoteIndex);
     return (
       <div className="arc-dc-page">
-        <ProductQuote
-          author="Simon Brooks"
-          title="Senior Vice President, Sales for North America"
-          backgroundImage="https://www2.arccorp.com/globalassets/products--participation/direct-connect/arc-dc-quote.png"
-          className=""
-          company="British Airways"
-          id="quote"
-          quote="British Airways is excited to be the first test partner for ARC's enhanced settlement functionality, which enables us to evolve our distribution strategy with  the travel agency channel. ARC has always been a trusted partner to British Airways, and we have valued  their flexibility, responsiveness and collaboration  during this testing process. These enhancements  will aid us in developing our relationships with  agencies, and they set the stage for our growing  distribution stratetgy in the U.S."
-        />
+        {visibleQuote}
+
         <div id="resources">
           <ProductLinks
             colClass="col-lg-4"
@@ -40,7 +68,7 @@ class App2 extends Component {
                 ),
                 icon: "https://www2.arccorp.com/globalassets/products--participation/direct-connect/ndc-icon-1.png",
                 link: "https://www2.arccorp.com/globalassets/datasheets/DirectConnectwithNDC.pdf",
-                direction: "down"
+                direction: "down",
               },
               {
                 alt: "Access ARC's NDC Media Library",
