b72759a35004af54de011efbb8aab5601c388686
Ian Fajardo <ifajardo@arccorp.com>
2024-01-31 09:57:04 -0500

    fix babel config

--- webpack.common.js
+++ webpack.common.js
@@ -26,14 +26,6 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: [
-              [
-                "@babel/plugin-transform-class-properties",
-                {
-                  loose: true,
-                },
-              ],
-            ],
           },
         },
       },
